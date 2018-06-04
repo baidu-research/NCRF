6c707d7ef23ebb19c7e8b2f49b2cf6b70d27072e
Yi Li <liyi17@baidu.com>
2018-06-04 16:26:54 -0700

    wsi/bin/camelyon16xml2json.py, coords, jsons added

---
+++ wsi/bin/camelyon16xml2json.py
@@ -0,0 +1,39 @@
+import sys
+import os
+import argparse
+import logging
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + '/../../')
+
+from wsi.data.annotation import Formatter  # noqa
+
+parser = argparse.ArgumentParser(description='Convert Camelyon16 xml format to\
+                                 internal json format')
+parser.add_argument('xml_path', default=None, metavar='XML_PATH', type=str,
+                    help='Path to the directory of Camelyon16 xml files')
+parser.add_argument('json_path', default=None, metavar='JSON_PATH', type=str,
+                    help='Path to the directory of json files')
+
+
+def run(args):
+    xml_files = [f for f in os.listdir(args.xml_path)]
+
+    if not os.path.exists(args.json_path):
+        os.mkdir(args.json_path)
+
+    for xml_file_name in xml_files:
+        pid = xml_file_name.split('.')[0]
+        inxml = os.path.join(args.xml_path, xml_file_name)
+        outjson = os.path.join(args.json_path, pid + '.json')
+        Formatter.camelyon16xml2json(inxml, outjson)
+
+
+def main():
+    logging.basicConfig(level=logging.INFO)
+
+    args = parser.parse_args()
+    run(args)
+
+
+if __name__ == '__main__':
+    main()
