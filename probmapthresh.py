4024854c5134e007c7c379f51aea55874f0e0141
udion <uddeshya.upa@gmail.com>
2018-07-16 10:39:58 +0530

    like master

--- probmapthresh.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-probmaps_dir = './sample_data/probmaps/'
-a = np.load(probmaps_dir+'test_012.npy')
-print(a.shape)
-print(np.max(a), np.min(a))
-
--- splitter.sh
+++
@@ -1,9 +0,0 @@
-for i in {1..16}
-do
- if [ $i -lt 10 ]
- then
-  tiffsplit ./sample_data/test_00$i.tif ./sample_data/split_files/test_00$i
- else
-  tiffsplit ./sample_data/test_0$i.tif ./sample_data/split_files/test_0$i
- fi
-done
