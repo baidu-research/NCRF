1c8987f73e2ccf717b605eb28913d47b4390c13d
Yi Li <yil8@uci.edu>
2018-07-23 10:46:28 -0700

    wsi/model/layers.py: NCRF algorithm details doc added

--- wsi/model/layers.py
+++ wsi/model/layers.py
@@ -16,7 +16,49 @@ class CRF(nn.Module):
         self.W = nn.Parameter(torch.zeros(1, num_nodes, num_nodes))
 
     def forward(self, feats, logits):
-        """Performing the CRF.
+        """Performing the CRF. Algorithm details is explained below:
+
+        Within the paper, I formulate the CRF distribution using negative
+        energy and cost, e.g. cosine distance, to derive pairwise potentials
+        following the convention in energy based models. But for implementation
+        simplicity, I use reward, e.g. cosine similarity to derive pairwise
+        potentials. So now, pairwise potentials would encourage high reward for
+        assigning (y_i, y_j) with the same label if (x_i, x_j) are similar, as
+        measured by cosine similarity, pairwise_sim. For
+
+        pairwise_potential_E = torch.sum(
+            probs * pairwise_potential - (1 - probs) * pairwise_potential,
+            dim=2, keepdim=True
+        )
+
+        This is taking the expectation of pairwise potentials using the current
+        marginal distribution of each patch being tumor, i.e. probs. There are
+        four cases to consider when taking the expectation between (i, j):
+
+        1. i=T,j=T; 2. i=N,j=T; 3. i=T,j=N; 4. i=N,j=N
+
+        probs is the marginal distribution of each i being tumor, therefore
+        logits > 0 means tumor and logits < 0 means normal. Given this, the
+        full expectation equation should be:
+
+        [probs * +pairwise_potential] + [(1 - probs) * +pairwise_potential] +
+                    case 1                            case 2
+        [probs * -pairwise_potential] + [(1 - probs) * -pairwise_potential]
+                    case 3                            case 4
+
+        positive sign rewards logits to be more tumor and negative sign rewards
+        logits to be more normal. But because of label compatibility, i.e. the
+        indicator function within equation 3 in the paper, case 2 and case 3
+        are dropped, which ends up being:
+
+        probs * pairwise_potential - (1 - probs) * pairwise_potential
+
+        In high level speaking, if (i, j) embedding are different, then
+        pairwise_potential, as computed as cosine similarity, would approach 0,
+        which then as no affect anyway. if (i, j) embedding are similar, then
+        pairwise_potential would be a positive reward. In this case,
+        if probs -> 1, then pairwise_potential promotes tumor probability;
+        if probs -> 0, then -pairwise_potential promotes normal probability.
 
         Args:
             feats: 3D tensor with the shape of
@@ -43,7 +85,7 @@ class CRF(nn.Module):
         unary_potential = logits.clone()
 
         for i in range(self.iteration):
-            # current Q
+            # current Q after normalizing the logits
             probs = torch.transpose(logits.sigmoid(), 1, 2)
             # taking expectation of pairwise_potential using current Q
             pairwise_potential_E = torch.sum(
